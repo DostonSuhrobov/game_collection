90b1e33373666e1a94c13cdaa338fed49c4b108b
DostonSuhrobov <doston.sanatovich@gmail.com>
2022-07-17 16:50:21 -0400

    style of card

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public sort: string | undefined;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
